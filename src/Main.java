b0e59dab45ec68a973a361f9302289868fc9a3c9
Green <toayush0451@gmail.com>
2026-02-20 09:57:04 +0530

    first Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("PALINDROME CHECKER");
+        System.out.println("Enter the String");
+
+        String str;
+
+    }
+}
